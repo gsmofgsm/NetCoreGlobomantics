2ba5dd072d1bb77daa27cb97cba6cb425f26cb36
Qing Ma <q.ma@ncontrol.cloud>
2021-03-16 23:14:37 +0100

    CDTagHelper with injected IRateService

---
+++ Globomantics/TagHelpers/CDTaghelper.cs
@@ -0,0 +1,34 @@
+﻿using Globomantics.Core.Models;
+using Globomantics.Services;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Globomantics.TagHelpers
+{
+    [HtmlTargetElement("cdrate")]
+    public class CDTagHelper : TagHelper
+    {
+        private readonly IRateService rateService;
+
+        public CDTagHelper(IRateService rateService)
+        {
+            this.rateService = rateService;
+        }
+
+        public string Title { get; set; }
+        public string MeterPercent { get; set; }
+        public CDTermLength TermLength { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var rate = rateService.GetCDRateByTerm(TermLength);
+
+            output.Content.SetHtmlContent(
+                $@"<div class=""meter"">
+                    <p>{ Title }</p>
+                    <div class=""progress"">
+                        <div class=""progress-bar bg-info"" style=""width: { MeterPercent }%""></div>
+                    </div>
+                </div>");
+        }
+    }
+}
