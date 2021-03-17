f124d43f21c0b5a73d6d5038482f505d398b89c6
Qing Ma <q.ma@ncontrol.cloud>
2021-03-17 22:20:38 +0100

    ModelValidationFilter replaces validation check in the controller

--- Globomantics/Controllers/LoanController.cs
+++ Globomantics/Controllers/LoanController.cs
@@ -28,13 +28,8 @@ namespace Globomantics.Controllers
         [HttpPost]
         public IActionResult Application(LoanDetails app)
         {
-            if (ModelState.IsValid)
-            {
-                loanService.CreateLoanApplication(app, Guid.NewGuid().ToString());
-                return RedirectToAction("Employment");
-            }
-
-            return View(app);
+            loanService.CreateLoanApplication(app, Guid.NewGuid().ToString());
+            return RedirectToAction("Employment");
         }
 
         [HttpGet]
@@ -46,13 +41,8 @@ namespace Globomantics.Controllers
         [HttpPost]
         public IActionResult Employment(Employment employment)
         {
-            if (ModelState.IsValid)
-            {
-                loanService.UpdateLoanEmployment(employment);
-                return RedirectToAction("Personal");
-            }
-
-            return View(employment);
+            loanService.UpdateLoanEmployment(employment);
+            return RedirectToAction("Personal");
         }
 
         [HttpGet]
@@ -64,13 +54,8 @@ namespace Globomantics.Controllers
         [HttpPost]
         public IActionResult Personal(Person person)
         {
-            if (ModelState.IsValid)
-            {
-                loanService.UpdateLoanPersonalInfo(person);
-                return RedirectToAction("Confirmation");
-            }
-
-            return View(person);
+            loanService.UpdateLoanPersonalInfo(person);
+            return RedirectToAction("Confirmation");
         }
 
         [HttpGet]
