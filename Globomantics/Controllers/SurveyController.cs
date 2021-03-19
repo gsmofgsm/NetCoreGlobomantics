c8264a67e07a84c5d552d73e0ba997ddade804ff
Qing Ma <q.ma@ncontrol.cloud>
2021-03-19 15:20:20 +0100

    Model Binder to parse unusually

---
+++ Globomantics/Controllers/SurveyController.cs
@@ -0,0 +1,21 @@
+﻿using Globomantics.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globomantics.Controllers
+{
+    public class SurveyController : Controller
+    {
+        public IActionResult Submission(List<Submission> submissions)
+        {
+            // In coming string: "q=1,4,Great Site&q=2,4,Great Service"
+
+            // TODO: Save submissions
+            return View();
+        }
+    }
+}
