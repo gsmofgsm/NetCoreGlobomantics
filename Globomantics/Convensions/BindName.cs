2d0b70d6ddb4e2199992ad84a03bd69fdcc01791
Qing Ma <q.ma@ncontrol.cloud>
2021-03-19 21:37:36 +0100

    Change Parameter binding convension by IParameterModelConvention

---
+++ Globomantics/Convensions/BindName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globomantics.Convensions
+{
+    public class BindName : Attribute, IParameterModelConvention
+    {
+        public string Name { get; set; }
+
+        public void Apply(ParameterModel parameter)
+        {
+            if (parameter.BindingInfo == null)
+            {
+                parameter.BindingInfo = new BindingInfo(); // do not try to change Binding info on a null object
+            }
+
+            parameter.BindingInfo.BinderModelName = Name;
+        }
+    }
+}
